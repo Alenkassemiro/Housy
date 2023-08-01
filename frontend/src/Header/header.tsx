0326d81920697c9c30ef66befe4d5305166270f2
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2023-08-01 14:06:30 -0300

    Add: background and landing page

---
+++ frontend/src/Header/header.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+import styles from "../Header/Header.module.scss";
+
+function Header() {
+  const router = useRouter();
+  return (
+    <div className={styles.main}>
+      <Link href={"/"}>
+        <img src="logo.svg" />
+      </Link>
+      <nav className={styles.nav}>
+        <ul className={styles.stroke}>
+          <li className={router.pathname === "/quemSomos" ? styles.active : ""}>
+            <Link href="/quemSomos">Quem somos</Link>
+          </li>
+          <li className={router.pathname === "/allProjects" ? styles.active : ""}>
+            <Link href="/allProjects">Nossas iniciativas</Link>
+          </li>
+          <li className={router.pathname === "/comoAjudar" ? styles.active : ""}>
+            <Link href="/comoAjudar">Como ajudar</Link>
+          </li>
+        </ul>
+      </nav>
+    
+    </div>
+  );
+}
+
+export default Header;
